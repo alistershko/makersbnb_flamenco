f14a06893a5ac48b6516e3dce9db26d845ab7c12
Louis <louistse5283@gmail.com>
2025-01-21 11:52:41 +0000

    Updated based on Frank's comments

--- model/booking.py
+++ model/booking.py
@@ -6,7 +6,7 @@ from extension import db
 class BookingModel(db.Model):
     __tablename__ = "bookings"
     id = db.Column(db.Integer, primary_key = True, autoincrement = True)
-    space_id = db.Column(db.Integer, db.ForeignKey("space.id"), nullable = False)
-    request_id = db.Column(db.Integer, db.ForeignKey("request.id"), nullable = False)
-    start_date = db.Column(db.String(200), nullable = False)
-    end_date = db.Column(db.String(200), nullable = False)
+    space_id = db.Column(db.Integer, db.ForeignKey("spaces.id"), nullable = False)
+    requester_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable = False)
+    booking_start_date = db.Column(db.Date, nullable = False)
+    booking_end_date = db.Column(db.Date, nullable = False)
