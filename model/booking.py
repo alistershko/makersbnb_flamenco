03b39787cb7666ceb28fbc9b5146a7c8935e9a4a
Louis <louistse5283@gmail.com>
2025-01-21 11:36:16 +0000

    updated model/booking.py

--- model/booking.py
+++ model/booking.py
@@ -1,1 +1,12 @@
-asldkfjlasfedj
+from sqlalchemy.engine import TupleResult
+from sqlalchemy.orm import backref
+
+from extension import db
+
+class BookingModel(db.Model):
+    __tablename__ = "bookings"
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    space_id = db.Column(db.Integer, db.ForeignKey("space.id"), nullable = False)
+    request_id = db.Column(db.Integer, db.ForeignKey("request.id"), nullable = False)
+    start_date = db.Column(db.String(200), nullable = False)
+    end_date = db.Column(db.String(200), nullable = False)
