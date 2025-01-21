2941b1b35bfc8559af9765a8c5ee22a578d7be2f
Alister <alistershko@gmail.com>
2025-01-21 14:49:54 +0000

    started writing spaces.py

--- blueprints/spaces.py
+++ blueprints/spaces.py
@@ -1,3 +1,32 @@
+from flask import Blueprint, render_template, redirect, url_for, flash, request, session
+from model.user import UserModel
+from extension import db
+from forms import *
+from werkzeug.security import generate_password_hash, check_password_hash
+
+spaces =Blueprint("name", __name__, url_prefix= "/spaces")
+
+@spaces.route('/make_a_new_space', methods=['GET', 'POST'])
+def MakeASpace():
+    space_form = SpaceForm()
+    if space_form.validate_on_sumbit():
+
+        if SpaceModel.query.filter_by(location=form.location.data).first():
+            flash("")
+        
+
+
+@spaces.route('/edit_an_existing_space', methods=['GET', 'POST'])
+
+@spaces.route('/delete_an_existing_space', methods=['GET', 'POST'])
+
+@spaces.route('/list_all_spaces', methods=['GET', 'POST'])
+
+@spaces.route('/list_one_space', methods=['GET', 'POST'])
+
+
+
+
 # make a new space
 # edit an existing space
 # delete an existing space
