904661c8aadf1b2c1d6ecbbaa578b64404fd017a
Louis <louistse5283@gmail.com>
2025-01-23 03:32:56 +0000

    Trial on user bookings & requests. Commented email_validator out on register form

--- forms/register_form.py
+++ forms/register_form.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, IntegerField, EmailField
 from wtforms.validators import DataRequired, Email, Length, EqualTo
-from email_validator import *
+# from email_validator import *
 
 class RegisterForm(FlaskForm):
     username = StringField("Username", validators=[DataRequired(), Length(min=3, max=20)])
